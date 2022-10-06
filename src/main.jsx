e58bff4065742d01e376cb4a173eb801af5c6636
Lucas Diaz <lucasgustavodiaz@hotmail.com>
2022-10-06 19:57:00 -0300

    refactor

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import './index.css'
 import { store, persistor } from './redux/store'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-// import { store, persistor } from './redux/store'
-// import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
