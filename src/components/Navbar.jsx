21b6599899204e3b5fd7892279f7078d379a8fc3
Lucas Diaz <lucasgustavodiaz@hotmail.com>
2022-10-08 19:38:57 -0300

    fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
-import { logo } from '../assets'
 import Cart from './Cart'
+import { logo } from '../assets'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import * as cartActions from '../redux/cart/cart-actions'
@@ -13,14 +13,12 @@ const Navbar = () => {
     }
   }
   return (
-    <div>
-      <div className="sticky top-0 right-0 z-[999] flex h-[75px] w-full items-center justify-between border-b border-[#e5edef] bg-white p-[10px]">
-        <Link to="/">
-          <img src={logo} alt="logo sitio" className="h-auto max-h-[100%] max-w-[200px]" onClick={handleToggleCart} />
-        </Link>
-        <div className="mr-[20px] flex p-4">
-          <Cart />
-        </div>
+    <div className="sticky top-0 right-0 z-[999] flex h-[75px] w-full items-center justify-between border-b border-[#e5edef] bg-white p-[10px]">
+      <Link to="/">
+        <img src={logo} alt="logo sitio" className="h-auto max-h-[100%] max-w-[200px]" onClick={handleToggleCart} />
+      </Link>
+      <div className="mr-[20px] flex p-4">
+        <Cart />
       </div>
     </div>
   )
